2f8c78dd6a09aa5078e172744a5670d2668e6fc9
michaellazzari-ctrl <michael.lazzari@gmail.com>
2026-06-30 16:20:54 +0200

    Enhance order_patch.js with project management methods
    Added methods to set and get project details, and updated JSON handling.

--- pos_project/static/src/app/models/order_patch.js
+++ pos_project/static/src/app/models/order_patch.js
@@ -8,12 +8,30 @@ patch(Order.prototype, {
     setup() {
         super.setup(...arguments);
 
-        this.project_id = null;
-        this.project_name = null;
+        this.project_id = this.project_id || null;
+        this.project_name = this.project_name || "";
+    },
+
+    setProject(project) {
+        if (!project) {
+            this.project_id = null;
+            this.project_name = "";
+            return;
+        }
+
+        this.project_id = project.id;
+        this.project_name = project.name;
+    },
+
+    getProjectId() {
+        return this.project_id;
+    },
+
+    getProjectName() {
+        return this.project_name;
     },
 
     export_as_JSON() {
-
         const json = super.export_as_JSON();
 
         json.project_id = this.project_id;
@@ -23,11 +41,18 @@ patch(Order.prototype, {
     },
 
     init_from_JSON(json) {
-
         super.init_from_JSON(...arguments);
 
-        this.project_id = json.project_id;
-        this.project_name = json.project_name;
+        this.project_id = json.project_id || null;
+        this.project_name = json.project_name || "";
+    },
+
+    export_for_printing() {
+        const receipt = super.export_for_printing();
+
+        receipt.project_name = this.project_name;
+
+        return receipt;
     },
 
 });
