022f1c7ff8920f82f6924f4c17205036cb689e6f
michaellazzari-ctrl <michael.lazzari@gmail.com>
2026-06-30 14:10:59 +0200

    Update POS Project addon manifest details

--- pos_project/__manifest__.py
+++ pos_project/__manifest__.py
@@ -1,16 +1,28 @@
-# -*- coding: utf-8 -*-
 {
-    'name': 'POS Project Addon',
-    'version': '1.0.0',
-    'category': 'Point of Sale',
-    'summary': 'Point of Sale Addon for enhanced functionality',
-    'author': 'Your Name',
-    'depends': ['point_of_sale'],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/pos_order_views.xml',
-        'views/pos_config_views.xml',
+    "name": "POS Project",
+    "version": "19.0.1.0.0",
+    "author": "Michael Lazzari",
+    "website": "https://github.com/michaellazzari-ctrl",
+    "license": "LGPL-3",
+    "category": "Point of Sale",
+    "summary": "Link POS orders to Projects",
+    "depends": [
+        "point_of_sale",
+        "project",
+        "account",
     ],
-    'installable': True,
-    'application': False,
+    "data": [
+        "security/ir.model.access.csv",
+        "views/pos_order_views.xml",
+        "views/pos_config_views.xml",
+    ],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "pos_project/static/src/js/**/*.js",
+            "pos_project/static/src/xml/**/*.xml",
+            "pos_project/static/src/scss/**/*.scss",
+        ],
+    },
+    "installable": True,
+    "application": False,
 }
